8de6225199883b7b8f0fd27067f6a8b39fd7d09e
Camila <acamilapazz@gmail.com>
2022-08-15 18:04:13 -0300

    Ejercicio 2

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-/*Crea un nuevo proyecto de Node, y dentro del package.json crea un script que muestre por pantalla
- "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo".*/ 
+/*Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
 
-console.log('Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo')
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)*/ 
+
+ var arreglo = ['Camila Paz', 21, true, Date(2001, 3, 3)];
+ const libro = {
+     titulo : 'Harry Potter y el prisionero de Azkaban',
+     autor : 'J.k. Rowling',
+     fecha : Date(2004, 5, 11),
+     url : 'https://es.wikipedia.org/wiki/Harry_Potter_y_el_prisionero_de_Azkaban_(pel%C3%ADcula)'
+ }
+ 
+ arreglo.push(libro);
+ console.log(arreglo);
